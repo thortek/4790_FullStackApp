d3ef40fb1e9cf4c1960bb082f8b0ec8d9c304c78
Thor Anderson <ta.anderson@gmail.com>
2022-09-19 14:59:40 -0600

    Fixed news aggregator issues

--- src/routes/aggregator/+page.server.js
+++ src/routes/aggregator/+page.server.js
@@ -1,16 +1,14 @@
 import { NEWS_APIKEY } from '$env/static/private'
+import { error } from '@sveltejs/kit'
 
 //https://api.thenewsapi.com/v1/news/headlines?locale=us&language=en&api_token=YOUR_API_TOKEN
 
 export async function load() {
     try {
-        const news_response = await fetch(`https://api.thenewsapi.com/v1/news/headlines?locale=us&language=en&api_token=${NEWS_APIKEY}`)
-        const newsData = await news_response.json()
-        console.log(newsData)
+        const news_response = await fetch(`https://api.thenewsapi.com/v1/news/top?locale=us&language=en&api_token=${NEWS_APIKEY}`)
+        const news_data = await news_response.json()
+        return { newsData: news_data.data}
     } catch (err) {
-        console.error(err)
-    }
-    return {
-        foo: 'bar'
+        throw error(404, 'Not found')
     }
 }
--- src/routes/aggregator/+page.svelte
+++ src/routes/aggregator/+page.svelte
@@ -1,20 +1,35 @@
 <script>
+  export let data, errors
+  $: console.log(data.newsData)
 
+  let storyContent = ''
+
+  function getStory(story) {
+    storyContent = story.description
+  }
 </script>
 
-<div class="drawer drawer-mobile">
+<div class="drawer">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col items-center justify-center">
       <!-- Page content here -->
-      <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-    
+      <label for="my-drawer-2" class="btn btn-primary drawer-button">Open drawer</label>
+      <div class="hero bg-base-200">
+        <p>{storyContent}</p>
+      </div>
     </div> 
     <div class="drawer-side">
       <label for="my-drawer-2" class="drawer-overlay"></label> 
       <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
         <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
+        {#each data.newsData as story (story.uuid) }
+        <li class="card" on:click={getStory(story)}>
+          <figure class="flex flex-col">
+            <img src={story.image_url} alt="Story pic"/>
+            <figcaption>{story.title}</figcaption>
+          </figure>
+        </li>
+        {/each}
       </ul>
     
     </div>
