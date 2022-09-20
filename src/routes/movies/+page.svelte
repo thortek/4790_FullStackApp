555943b60c3b8c5617ef433cb287f9013dc5cc0d
Thor Anderson <ta.anderson@gmail.com>
2022-09-20 08:40:58 -0600

    Wrap with if/else block to deal with null form

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,33 +1,33 @@
 <script>
 	export let form, errors
-	//$: console.log(data, form);
-	//let firstMovie = data.movies.Search[0];
+	//$: console.log(form)
 </script>
 
 {#if errors?.title}
-<p class="error">{errors.title}</p>
+	<p class="error">{errors.title}</p>
 {/if}
 
 <main class="flex flex-wrap justify-center">
-<!-- {#if form.length === 0}
+	{#if !form}
 		<div class="card">
 			<div class="card-body">
 				<h2 class="card-title">No movies loaded. Enter a valid movie title in the search box.</h2>
 			</div>
 		</div>
-	{/if} -->
-	{#each form as movie}
-		<div class="card w-96 bg-base-100 shadow-xl m-4">
-			<figure>
-				<img src={movie.Poster} alt="Movie poster" />
-			</figure>
-			<div class="card-body">
-				<h2 class="card-title">{movie.Title}</h2>
-				<p>{movie.Year}</p>
-				<div class="card-actions justify-end">
-					<button class="btn btn-primary">Buy Now</button>
+	{:else}
+		{#each form as movie}
+			<div class="card w-96 bg-base-100 shadow-xl m-4">
+				<figure>
+					<img src={movie.Poster} alt="Movie poster" />
+				</figure>
+				<div class="card-body">
+					<h2 class="card-title">{movie.Title}</h2>
+					<p>{movie.Year}</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	{/each}
+		{/each}
+	{/if}
 </main>
