fd970db1dbde05b00cd9d1ba1daeee94c044c309
Thor Anderson <ta.anderson@gmail.com>
2022-10-05 15:38:20 -0600

    Added method to Getting Started button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
+<script>
+  import { goto } from '$app/navigation'
+
+  function navigate() {
+    goto('/dashboard')
+  }
+</script>
+
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">Hello there</h1>
         <p class="py-6">Welcome to my DGM 3790 SvelteKit Project!  Check out the menu items to see all the cool stuff I've built.</p>
-        <button class="btn btn-primary">Get Started</button>
+        <button class="btn btn-primary" on:click="{navigate}">Get Started</button>
       </div>
     </div>
   </div>
