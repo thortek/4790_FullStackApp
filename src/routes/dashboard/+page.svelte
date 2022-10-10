e9aaf6eab428250041ac5aa9e833a09acada1f12
Thor Anderson <ta.anderson@gmail.com>
2022-10-10 15:39:06 -0600

    Added some charts to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,17 +1,23 @@
 <script>
-	export let data;
-	export const prerender = true;
+	import { Line, Doughnut } from 'svelte-chartjs'
+	import 'chart.js/auto'
+	import { chartData } from '../../lib/components/chartData.js'
+	import { donutData } from '../../lib/components/donutData.js'
+
+	export let data
+	export const prerender = true
 	// $: console.log(Object.values(data));
-	let pics = Object.values(data);
+	let pics = Object.values(data)
 </script>
 
 <div class="min-h-screen flex flex-wrap">
-	<progress class="progress w-56" value="0" max="100" />
-	<div class="carousel carousel-center p-4 max-h-96 max-w-lg space-x-4 bg-neutral rounded-box m-1">
+	<div class="carousel carousel-center p-4 max-h-96 max-w-md space-x-4 bg-neutral rounded-box m-1">
 		{#each pics as pic}
 			<div class="carousel-item">
 				<img class="rounded-box" src={pic.url} alt="Nasa pic" />
 			</div>
 		{/each}
 	</div>
+	<Line data={chartData} />
+	<Doughnut data={donutData} options={{ responsive: true }} />
 </div>
