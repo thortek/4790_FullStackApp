27cec86994754df29a0fe8eec694e79d06a914f9
Thor Anderson <ta.anderson@gmail.com>
2022-10-17 15:02:13 -0600

    Fixed up layout and modal toggling

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -1,38 +1,81 @@
 <script>
-    import { faker } from '@faker-js/faker'
+	import { faker } from '@faker-js/faker'
+
+  let showModal = false
+
+  function toggleModal() {
+    showModal = !showModal
+  }
 </script>
 
-<div class="stats shadow">
-  
-    <div class="stat">
-      <div class="stat-figure text-primary">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-      </div>
-      <div class="stat-title">Total Likes</div>
-      <div class="stat-value text-primary">{faker.random.numeric(2)}.{faker.random.numeric()}K</div>
-      <div class="stat-desc">{faker.random.numeric(2)}% more than last month</div>
-    </div>
-    
-    <div class="stat">
-      <div class="stat-figure text-secondary">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-      </div>
-      <div class="stat-title">Page Views</div>
-      <div class="stat-value text-secondary">2.6M</div>
-      <div class="stat-desc">21% more than last month</div>
-    </div>
-    
-    <div class="stat">
-      <div class="stat-figure text-secondary">
-        <div class="avatar online">
-          <div class="w-16 rounded-full">
-            <img src="https://placeimg.com/128/128/people" alt="Statistics person"/>
-          </div>
-        </div>
-      </div>
-      <div class="stat-value">86%</div>
-      <div class="stat-title">Tasks done</div>
-      <div class="stat-desc text-secondary">31 tasks remaining</div>
-    </div>
-    
-  </div>
+<div class="stats shadow-xl m-4">
+	<div class="stat">
+		<div class="stat-figure text-primary">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-8 h-8 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+				/></svg
+			>
+		</div>
+		<div class="stat-title">Total Likes</div>
+		<div class="stat-value text-primary">{faker.random.numeric(2)}.{faker.random.numeric()}K</div>
+		<div class="stat-desc">{faker.random.numeric(2)}% more than last month</div>
+	</div>
+
+	<div class="stat">
+		<div class="stat-figure text-secondary">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-8 h-8 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M13 10V3L4 14h7v7l9-11h-7z"
+				/></svg
+			>
+		</div>
+		<div class="stat-title">Page Views</div>
+		<div class="stat-value text-secondary">2.6M</div>
+		<div class="stat-desc">21% more than last month</div>
+	</div>
+
+	<div class="stat">
+		<div class="stat-figure text-secondary">
+			<div class="avatar online">
+				<div class="w-16 rounded-full">
+					<!-- svelte-ignore a11y-invalid-attribute -->
+					<a href="#" on:click={toggleModal}>
+						<img src="https://placeimg.com/128/128/people" alt="Statistics person" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="stat-value">86%</div>
+		<div class="stat-title">Tasks done</div>
+		<div class="stat-desc text-secondary">31 tasks remaining</div>
+	</div>
+
+	<!-- Put this part before </body> tag -->
+	<!-- <input type="checkbox" id="my-modal" class="modal-toggle" /> -->
+	<div class="modal" class:modal-open={showModal}>
+		<div class="modal-box">
+			<h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+			<p class="py-4">
+				You've been selected for a chance to get one year of subscription to use Wikipedia for free!
+			</p>
+			<div class="modal-action">
+				<label for="my-modal" class="btn" on:click={toggleModal}>Yay!</label>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -11,27 +11,27 @@
 	// $: console.log(Object.values(data))
 	let pics = Object.values(data);
 	const companyName = faker.company.name()
-	$: showModal = ''
+	
 </script>
 
-<div class="flex flex-wrap">
-	<div class="carousel carousel-center p-4 max-h-96 m-4 space-x-4 bg-neutral rounded-box">
+<div class="flex flex-wrap bg-slate-300">
+	<div class="carousel carousel-center p-4 max-h-96 max-w-md m-4 space-x-4 bg-neutral rounded-box">
 		{#each pics as pic}
 			<div class="carousel-item">
 				<img class="rounded-box" src={pic.url} alt="Nasa pic" />
 			</div>
 		{/each}
 	</div>
-	<div class="w-1/2 flex items-center bg-slate-100 shadow-xl">
+	<div class="w-96 flex items-center">
 		<Line data={chartData} />
 	</div>
-	<div class="w-1/2 bg-slate-100 shadow-xl">
+	<div class="w-96">
 		<Doughnut class="p-16" data={donutData} />
 	</div>
 	<!-- Stats element begins here-->
 	<Stats />
 	<!-- Mission statement here -->
-	<div class="card w-full bg-base-100 shadow-xl" on:click={() => showModal = 'modal-open'}>
+	<div class="card w-full bg-base-100 shadow-xl">
 		<div class="card-body">
 			<h2 class="card-title">{companyName} Mission Statement</h2>
 			<p>
@@ -42,14 +42,4 @@
 		</div>
 	</div>
 
-	<!-- Put this part before </body> tag -->
-	<input type="checkbox" id="my-modal" class="modal-toggle" />
-	<label for="my-modal" class="modal cursor-pointer {showModal}" on:click={() => showModal = ''}>
-		<label class="modal-box relative" for="my-modal">
-			<h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-			<p class="py-4">
-				You've been selected for a chance to get one year of subscription to use Wikipedia for free!
-			</p>
-		</label>
-	</label>
 </div>
