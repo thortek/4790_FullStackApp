b45a4646362333bfc1486894ded65d1cf40bf0e9
Thor Anderson <ta.anderson@gmail.com>
2022-10-24 15:38:46 -0600

    Simple way to use svelte store for the user theme

--- src/lib/stores/theme.js
+++ src/lib/stores/theme.js
@@ -1,6 +1,13 @@
 import { writable } from 'svelte/store'
+import { browser } from '$app/environment'
 
 const defaultValue = 'light'
-const initialValue = 'dark'
+const initialValue = browser ? window.localStorage.getItem('theme') : defaultValue
 
 export const theme = writable(initialValue)
+
+theme.subscribe(value => {
+    if (browser) {
+        window.localStorage.setItem('theme', value)
+    }
+})
