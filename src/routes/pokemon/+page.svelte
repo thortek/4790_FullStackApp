3ac0b265b9b718b7cc4e12d618f9896082e09e5b
Thor Anderson <ta.anderson@gmail.com>
2022-10-31 15:29:09 -0600

    Got onMount working.  Add lazy image loading later

---
+++ src/routes/pokemon/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from 'svelte'
+
+	let allPokemon = []
+
+	onMount(async () => {
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1200`)
+		const pokeData = await response.json()
+		allPokemon = pokeData.results
+	})
+</script>
+
+<h1 class="text-xl">Pokemon Grid</h1>
+
+<div class="w-full grid grid-cols-8 gap-2 p-2">
+	{#each allPokemon as pokemon}
+		<figure>
+			<img src={`https://via.Placeholder.com/150/abcdef`} alt={pokemon.name} />
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	{:else}
+		<p>Loading...</p>
+	{/each}
+</div>
