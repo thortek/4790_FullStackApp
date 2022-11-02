f337952d8b0f25c9e48d96ae101e555d3e7d1206
Thor Anderson <ta.anderson@gmail.com>
2022-11-02 15:33:54 -0600

    Image lazy loading on pokemon route

---
+++ src/lib/components/Image/ImageLoader.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let src
+    export let alt
+  
+    import IntersectionObserver from './IntersectionObserver.svelte'
+    import Image from './Image.svelte'
+    
+    let nativeLoading = false
+    
+  </script>
+  
+  <IntersectionObserver once={true} let:intersecting={intersecting}>
+    {#if intersecting || nativeLoading}
+      <Image {alt} {src} />
+    {/if}
+  </IntersectionObserver>
--- src/routes/pokemon/+page.svelte
+++ src/routes/pokemon/+page.svelte
@@ -1,21 +1,26 @@
 <script>
 	import { onMount } from 'svelte'
+	import ImageLoader from '../../lib/components/Image/ImageLoader.svelte'
 
 	let allPokemon = []
 
 	onMount(async () => {
 		const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1200`)
 		const pokeData = await response.json()
-		allPokemon = pokeData.results
+
+		allPokemon = pokeData.results.map(pokemon => {
+			pokemon.id = pokemon.url.slice(34, pokemon.url.lastIndexOf('/'))
+			return pokemon
+		})
 	})
 </script>
 
 <h1 class="text-xl">Pokemon Grid</h1>
 
-<div class="w-full grid grid-cols-8 gap-2 p-2">
+<div class="w-full grid grid-cols-6 gap-2 p-2">
 	{#each allPokemon as pokemon}
 		<figure>
-			<img src={`https://via.Placeholder.com/150/abcdef`} alt={pokemon.name} />
+			<ImageLoader src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`} alt={pokemon.name}></ImageLoader>
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	{:else}
