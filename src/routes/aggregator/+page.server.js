81021fbb8b9775bcd3bc87ca1ae8c5c2e2b08709
Thor Anderson <ta.anderson@gmail.com>
2022-12-07 14:40:28 -0700

    Switched back to newsapi.org on aggregator route

--- src/routes/aggregator/+page.server.js
+++ src/routes/aggregator/+page.server.js
@@ -1,42 +1,12 @@
-//import { NEWS_APIKEY } from '$lib/server/secrets.js'
+import { NEWS_APIKEY } from '$env/static/private'
 import { error } from '@sveltejs/kit'
-
-//https://api.thenewsapi.com/v1/news/headlines?locale=us&language=en&api_token=YOUR_API_TOKEN
 
 export async function load() {
     try {
-        const news_response = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
+        const news_response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_APIKEY}`)
         const news_data = await news_response.json()
-        const allTopStories = await getAllTopStories(news_data)
-        return { topStories: allTopStories }
+        return { topStories: news_data.articles }
     } catch (err) {
         throw error(404, 'Not found')
     }
-}
-
-async function getAllTopStories(topStoryIDArray) {
-    let topStories = []
-    /*     // console.log(pageOne)
-        const totalResults = parseInt(pageOne.totalResults)
-        //console.log(totalResults)
-        if(totalResults === 0) return // should probably let user know "No results"
-        if (totalResults > 0 && totalResults < 11) {
-            return pageOne.Search // return just the array of the results
-        }
-        allTheMovies = [...pageOne.Search]
-        // figure out how many times to loop
-        let counter = Math.ceil(totalResults / 10) */
-    const firstTen = topStoryIDArray.slice(0, 30)
-    for (const element of firstTen) {
-        //console.log(element, index)
-        try {
-            const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`)
-            const article = await response.json()
-            topStories.push(article)
-        }
-        catch (err) {
-            console.error(err)
-        }
-    }
-    return topStories
 }
--- src/routes/aggregator/+page.svelte
+++ src/routes/aggregator/+page.svelte
@@ -1,37 +1,35 @@
 <script>
-  import sanitizeHtml from "sanitize-html";
-  export let data, errors
-  $: console.log(data.topStories)
+	export let data, errors
+	//$: console.log(data.topStories)
 
-  let storyContent = ''
+	let storyContent = ''
 
-  function getStory(story) {
-    storyContent = sanitizeHtml(story.text)
-  }
+	function getStory(story) {
+		storyContent = story.content
+	}
 </script>
 
-<div class="drawer">
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col items-center justify-center">
-      <!-- Page content here -->
-      <label for="my-drawer-2" class="btn btn-primary drawer-button">Open drawer</label>
-      <div class="hero bg-base-200">
-        <p>{@html storyContent}</p>
-      </div>
-    </div> 
-    <div class="drawer-side">
-      <label for="my-drawer-2" class="drawer-overlay"></label> 
-      <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-        <!-- Sidebar content here -->
-      {#each data.topStories as story }
-        <li class="card" on:click={getStory(story)}>
-          <figure class="flex flex-col">
-            <!-- <img src={story.image_url} alt="Story pic"/> -->
-            <figcaption>{story.title}</figcaption>
-          </figure>
-        </li>
-        {/each}
-      </ul>
-    
-    </div>
-  </div>
+<div class="drawer overflow-x-hidden overflow-y-auto">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+		<!-- Page content here -->
+		<label for="my-drawer-2" class="btn btn-primary drawer-button">Open drawer</label>
+		<div class="flex content-center justify-center w-1/2 ">
+      <h4 class="bg-slate-100">{storyContent}</h4>
+		</div>
+	</div>
+	<div class="drawer-side ">
+		<label for="my-drawer-2" class="drawer-overlay " />
+		<ul class="menu p-4 w-80 bg-base-100 text-base-content">
+			<!-- Sidebar content here -->
+			{#each data.topStories as story}
+				<li class="card" on:click={getStory(story)}>
+					<figure class="flex flex-col bg-yellow-500 m-1">
+						<img src={story.urlToImage} alt="Story pic"/>
+						<figcaption>{story.title}</figcaption>
+					</figure>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
