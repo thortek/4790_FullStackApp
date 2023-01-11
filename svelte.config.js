0d815ff5d8f9040584678912c51ec5deaa932da0
Thor Anderson <ta.anderson@gmail.com>
2023-01-11 14:55:54 -0700

    Trying to get amplify hosting to work

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import { adapter } from 'sveltekit-adapter-aws';
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			autoDeploy: true,
+			FQDN: 'main.d1yxpnooq62isg.amplifyapp.com',
+			stackName: '4790-backend'
+		})
 	},
 
 };
