77bf7720aa5997e2b564ce19b6b57834cc70497e
Thor Anderson <ta.anderson@gmail.com>
2023-01-12 17:44:27 -0700

    Try with new custom domain, no stackName provided

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			autoDeploy: true,
-			FQDN: 'main.d1yxpnooq62isg.amplifyapp.com',
-			stackName: 'amplify-4790backend-staging-220634'
+			FQDN: 'www.thoranderson.site',
 		})
 	},
 
