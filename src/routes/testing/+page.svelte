66829b956277b1b415e23a67060b1b3235ef86aa
Thor Anderson <ta.anderson@gmail.com>
2023-02-13 15:44:44 -0700

    Example reading and deleting Posts

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -1,6 +1,12 @@
 <script>
-	import { DataStore } from 'aws-amplify'
+	import { DataStore, Predicates } from 'aws-amplify'
     import { Post, PostStatus } from '../../models'
+
+	export let data
+
+	let search = ''
+
+	const allPosts = data.allPosts
 
 	const createPost = async () => {
 		await DataStore.save(
@@ -11,8 +17,38 @@
 			})
 		)
 	}
+
+	const deletePosts = async () => {
+		await DataStore.delete(Post, Predicates.ALL)
+	}
 </script>
 
-<div class="w-fit">
+<div class="w-fit m-4">
 	<button class="btn" on:click={createPost}>Save Post</button>
+	<button class="btn" on:click={deletePosts}>Delete All Posts</button>
+	<div class="my-4">
+		<input type="search" placeholder="Search..." bind:value={search} class="input input-bordered input-primary"/>
+		</div>
+	<table class="table w-full">
+		<thead>
+			<tr>
+			<th>Id</th>
+			<th>Title</th>
+			<th>Status</th>
+			<th>Rating</th>
+			<th>Content</th>
+		</tr>
+			</thead>
+			<tbody>
+				{#each allPosts as post}
+					<tr>
+						<td>{post.id}</td>
+						<td>{post.title}</td>
+						<td>{post.status}</td>
+						<td>{post.rating}</td>
+						<td>{post.content}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 </div>
