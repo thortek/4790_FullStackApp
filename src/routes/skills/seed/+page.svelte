28f5815fa92ba12b7c1dd4bbaa63a5074058fe19
Thor Anderson <ta.anderson@gmail.com>
2023-02-15 15:43:43 -0700

    Added add items button to table

--- src/routes/skills/seed/+page.svelte
+++ src/routes/skills/seed/+page.svelte
@@ -24,12 +24,19 @@
 <table class="table w-full overflow-y-auto">
     <thead>
         <tr>
+            <th>
+            <div class="flex items-center">
+                <p>Select</p>
+                <button class="btn btn-primary mx-2">Add Items</button></div>
+
+                </th>
             <th class="text-left">Skill</th>
             <th class="text-left">Id</th>    
         </thead>
     <tbody>
         {#each skills as skill}
             <tr>
+                <td><input type="checkbox"/></td>
                 <td>{skill.name}</td>
                 <td>{skill.id}</td>
             </tr>
