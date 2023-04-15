59db484fad2e14f0459204de8416177077851503
Thor Anderson <ta.anderson@gmail.com>
2023-04-15 10:57:44 -0600

    Upload route now modal. Allow 1 pic per user

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,42 +1,48 @@
 <script>
 	import { enhance } from '$app/forms'
 	import { theme } from '$lib/stores/theme'
-	import { Auth, DataStore } from 'aws-amplify'
+	import { Auth, DataStore, Storage } from 'aws-amplify'
 	import { goto } from '$app/navigation'
 	import { localUser } from '$lib/stores/localUser'
+	import { onMount } from 'svelte'
+	import ProfilePicModal from '$lib/components/ProfilePicModal/ProfilePicModal.svelte'
 
 	let themeOptions = ['light', 'dark', 'cupcake', 'aqua', 'dracula', 'winter']
 
 	let selectedTheme
-	//let localUser = JSON.parse($user)
-	//console.log(localUser)
+	let open = false
 
 	$: if (selectedTheme && selectedTheme !== 'Theme') $theme = selectedTheme
 
-	console.log('About to try and authenticate user...')
-	Auth.currentAuthenticatedUser()
-		.then((user) => {
-			console.log('User is authenticated...', user.attributes.email)
-			$localUser = user
-		})
-		.catch((err) => {
-			console.log(err)
-			$localUser = null
-		})
+	onMount(async () => {
+		Auth.currentAuthenticatedUser()
+			.then(async (user) => {
+				console.log('User is authenticated as', user.attributes.email)
+				$localUser = user
+				try {
+					const imageFile = await Storage.list('profilePic/', { level: 'protected', pageSize: 1 })
+					const signedURL = await Storage.get(imageFile.results[0].key, { level: 'protected' })
+					console.log(signedURL)
+					$localUser.profilePic = signedURL
+				} catch (err) {
+					console.log('It is likely that the user has not uploaded a profile pic yet.')
+				}
+			})
+			.catch((err) => {
+				console.log(err)
+				$localUser = null
+			})
+	})
 
 	async function logInOut() {
-		if (localUser) {
-			try {
-				await Auth.signOut()
-				//if (DataStore.state === 'Running') await DataStore.clear()
-				await DataStore.clear()
-				$localUser = null
-				goto('/')
-			} catch (error) {
-				console.log('error signing out: ', error)
-			}
-		} else {
+		try {
+			await Auth.signOut()
+			//if (DataStore.state === 'Running') await DataStore.clear()
+			await DataStore.clear()
+			$localUser = null
 			goto('/auth/login')
+		} catch (error) {
+			console.log('error signing out: ', error)
 		}
 	}
 </script>
@@ -61,7 +67,7 @@
 		<a href="/movies" class="btn btn-ghost normal-case text-xl">Movies</a>
 		<a href="/dashboard" class="btn btn-ghost normal-case text-xl">Dashboard</a>
 		<a href="/aggregator" class="btn btn-ghost normal-case text-xl">News Aggregator</a>
-		
+
 		{#if $localUser}
 			<a href="/skills" class="btn btn-ghost normal-case text-xl">Skills</a>
 		{/if}
@@ -71,11 +77,19 @@
 		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 		<!-- svelte-ignore a11y-label-has-associated-control -->
 
-		<div class="avatar placeholder" class:online={localUser}>
-			<div class="bg-neutral-focus text-neutral-content rounded-full w-16">
-				<label tabindex="0" class="btn m-1">{$localUser?.attributes?.name ?? '?'}</label>
+		<div class="avatar" class:online={$localUser}>
+			<div class="bg-neutral-focus text-neutral-content rounded-xl w-24">
+				{#if $localUser?.profilePic}
+					<!-- svelte-ignore a11y-invalid-attribute -->
+
+					<img src={$localUser?.profilePic} alt="avatar" tabindex="0" />
+				{:else}
+					<label tabindex="0" class="block w-full h-full text-center"
+						>{$localUser?.attributes?.name ?? 'Login'}</label>
+				{/if}
 			</div>
 		</div>
+
 		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 		<ul
 			tabindex="0"
@@ -83,8 +97,9 @@
 			{#if $localUser}
 				<li>Welcome {$localUser.attributes.name}!</li>
 			{/if}
-			<li><a href="/user/profile">Profile</a></li>
-			<li><a href="/user/settings">Settings</a></li>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<li><a on:click={() => open = !open}>ProfilePic</a></li>
 			<li>
 				<select bind:value={selectedTheme} class="select w-full max-x-xs">
 					<option disabled selected>Theme</option>
@@ -101,3 +116,5 @@
 		</ul>
 	</div>
 </header>
+
+<ProfilePicModal isModalOpen={open} />
